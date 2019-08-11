777d083f2921b560fc4f093b79710e3bef7eaf24
alessa02 <53171831+alessa02@users.noreply.github.com>
2019-08-11 22:24:30 +1000

    Add files via upload

--- cleaning.py
+++ cleaning.py
@@ -1,22 +1,33 @@
 import pandas as pd
 import spacy
-
+import re
+import string
+from spacy.lang.en import stop_words
+file_name = 'competition_test_bodies.csv'
 def load_body_sentence(file):
     nlp = spacy.load('en_core_web_lg')
     df=pd.read_csv(file)
-    ###df=pd.read_csv('train_bodies.csv')
+    #df=pd.read_csv('test_bodies.csv')
     body_sentence = {}
     for row in df.iterrows():
         if row[1]["Body ID"] not in body_sentence:
             line = row[1]["articleBody"]
-            nlpDoc = nlp(line)
-            temp = [(" ".join([token.lemma_ for token in sent if not \
-                               token.is_punct and not token.is_stop and \
-                               not token.is_space])).lower() for sent in nlpDoc.sents]
+            #print(f"Line: {line}\n")
+            text = """""".join(ele if ele not in ['\\' , '\a', '\b', '\f', '\r' '\t', '\v'] \
+                               and not ele.isdigit() else ' ' for ele in line)
+            #print(f"Pre-processed text: {text}\n")
+            nlpDoc = nlp(text)
+            temp = [(" ".join([re.sub(r'[\W+\d]','', token.text) for token in sent if \
+                               re.sub(r'[\W+\d]','', token.text) != '' and \
+                               re.sub(r'[\W+\d]','', token.text) not in (stop_words.STOP_WORDS) and \
+                               len(re.sub(r'[\W+\d]', '', token.text)) > 2])).lower()
+                               for sent in nlpDoc.sents]
+            #print(f"Temp tokens: {temp}\n")
+            nw_temp = [" ".join(tk.lemma_ for tk in sent) for item in temp for sent in [nlp(item)]]
             temp = ','.join([x for x in temp if x != ''])
+            #print(f"Final: {temp}\n")
             body_sentence[row[1]["Body ID"]] = temp
     return body_sentence
-        
 
 
 
