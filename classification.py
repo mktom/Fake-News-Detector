669e2ff2e5377832ed639da066d07ea5b1a9cc7e
alessa02 <53171831+alessa02@users.noreply.github.com>
2019-08-12 21:49:23 +1000

    Update classification.py

--- classification.py
+++ classification.py
@@ -14,6 +14,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 from score import report_score
 from sklearn.linear_model import LogisticRegression
+from xgboost.sklearn import XGBClassifier
 #_____________________
 ##from pandas.plotting import scatter_matrix
 ##import matplotlib.pyplot as plt
@@ -50,80 +51,39 @@ trainY = list((stage2_frame.values[:,5]).astype('int64'))
 
 
 #method1: 2 classification for all 4 class
-import xgboost as xgb
-def train_relatedness_classifier(trainX, trainY):
-    xg_train = xgb.DMatrix(trainX, label=trainY)
-    # setup parameters for xgboost
-    param = {}
-    # use softmax multi-class classification
-    param['objective'] = 'binary:logistic'
-    # scale weight of positive examples
-    param['eta'] = 0.1
-    param['max_depth'] = 6
-    param['silent'] = 1
-    param['nthread'] = 20
-
-    num_round = 1000
-    relatedness_classifier = xgb.train(param, xg_train, num_round)
-    return relatedness_classifier
 def method1():
-    all_trainX = copy.deepcopy(trainX_all)
-    all_trainY = list((combined_dataframe.values[:,4]).astype('int64'))
-    relatedness_classifier = train_relatedness_classifier(all_trainX, all_trainY)
+    alltrainX = copy.deepcopy(trainX_all)
+    alltrainY = list((combined_dataframe.values[:,4]).astype('int64'))
+    xg1 = XGBClassifier(learning_rate=0.1, n_estimators=1000, max_depth=6,eta = 0.1,\
+                             objective="binary:logistic", subsample=0.9, colsample_bytree=0.8)
+    xg1.fit(np.array(alltrainX),np.array(alltrainY))
+    #relatedness_classifier = train_relatedness_classifier(trainX, trainY)
 
     relatedTrainX = trainX
-    related_frame = combined_dataframe[combined_dataframe['first']==0]
+    related_frame = combined_dataframe[combined_dataframe['first']==0].copy()
     related_frame["discussY"] = related_frame['Stance'].apply({'discuss':1,'agree':0,'disagree':0}.get)
     relatedTrainY = list((related_frame.values[:,6]).astype('int64'))
-    discuss_classifier = train_relatedness_classifier(relatedTrainX, relatedTrainY)
+    xg2 = XGBClassifier(learning_rate=0.1, n_estimators=1000, max_depth=6,eta = 0.1,\
+                             objective="binary:logistic", subsample=0.9, colsample_bytree=0.8)
+    xg2.fit(np.array(relatedTrainX), np.array(relatedTrainY))
 
     agree = combined_dataframe[combined_dataframe['second']>1].index.tolist()
     agreeTrainX = []
     for i in agree:
         agreeTrainX.append(trainX_all[i])
-    agree_frame = combined_dataframe[combined_dataframe['second']>1]
+    agree_frame = combined_dataframe[combined_dataframe['second']>1].copy()
     agree_frame["agreeY"] = agree_frame['Stance'].apply({'agree':1,'disagree':0}.get)
     agreeTrainY = (agree_frame.values[:,6]).astype('int64')
-    agree_classifier = train_relatedness_classifier(agreeTrainX, agreeTrainY)
-    return relatedness_classifier,discuss_classifier,agree_classifier
-def prediction1():
-    f = open('test_feature.pkl', 'rb')
-    textX = pickle.load(f)
-    f.close()
-    f = open('relatedness_classifier.pkl', 'rb')
-    relatedness_classifier = pickle.load(f)
-    f.close()
-    f = open('discuss_classifier.pkl', 'rb')
-    discuss_classifier = pickle.load(f)
-    f.close()
-    f = open('agree_classifier.pkl', 'rb')
-    agree_classifier = pickle.load(f)
-    f.close()
-    xg_test = xgb.DMatrix(textX)
-    relatedness_pred = relatedness_classifier.predict(xg_test);
-    discuss_pred = discuss_classifier.predict(xg_test)
-    agree_pred = agree_classifier.predict(xg_test)
-
-    ret, scores = [], []
-    for (pred_relate, pred_discuss, pred_agree) in zip(relatedness_pred, discuss_pred, agree_pred):
-        scores.append((pred_relate, pred_discuss, pred_agree))
-        if pred_relate >= 0.5:
-            ret.append('unrelated')
-        elif pred_discuss >= 0.5:
-            ret.append('discuss')
-        elif pred_agree >= 0.5:
-            ret.append('agree')
-        else:
-            ret.append('disagree')
-    return relatedness_pred,discuss_pred,agree_pred,ret,scores
-
+    lr= LogisticRegression(C = 1.0,penalty = 'l2',solver = 'lbfgs')
+    lr.fit(agreeTrainX, agreeTrainY)
+    return xg1,xg2,lr
 #method2:  2 classification + 3 classification
 def binary():
     lr= LogisticRegression(C = 1.0,penalty = 'l2',solver = 'lbfgs')
     metric_all = pd.DataFrame()
     metric = cross_val_score(lr, trainX_all,trainy_all, cv=10, scoring='f1',verbose = 1)
-    metric.sort()
-    metric_all['glm'] = metric[::-1]
+    #metric.sort()
+    metric_all['LogisticRegression'] = metric[::-1]
 
 
     X_train1, X_test1, y_train1, y_test1 = train_test_split(trainX_all,trainy_all, test_size = 0.3, random_state = 0)
@@ -131,7 +91,7 @@ def binary():
     metric = cross_val_score(svc, X_test1,y_test1, cv=10, scoring='f1',verbose = 1)
     #[Parallel(n_jobs=1)]: Using backend SequentialBackend with 1 concurrent workers.
     #[Parallel(n_jobs=1)]: Done  10 out of  10 | elapsed:  1.8min finished
-    metric.sort()
+    #metric.sort()
     metric_all['svm'] = metric[::-1]
 
     dt = DecisionTreeClassifier(criterion="gini",max_depth=11)
@@ -139,46 +99,40 @@ def binary():
     metric = cross_val_score(dt, X_train1,y_train1, cv=10, scoring='f1',verbose = 1)
     #[Parallel(n_jobs=1)]: Using backend SequentialBackend with 1 concurrent workers.
     #[Parallel(n_jobs=1)]: Done  10 out of  10 | elapsed:  2.2min finished
-    metric.sort()
-    metric_all['tree'] = metric[::-1]
-
-    RF = RandomForestClassifier(n_estimators=30, criterion='gini', random_state=0)
-    metric = cross_val_score(RF,trainX_all,trainy_all, cv=10, scoring='f1',verbose = 1)
-    #[Parallel(n_jobs=1)]: Using backend SequentialBackend with 1 concurrent workers.
-    #[Parallel(n_jobs=1)]: Done  10 out of  10 | elapsed:  4.6min finished
-    metric.sort()
-    metric_all['RF30'] = metric[::-1]
-
-    RF = RandomForestClassifier(n_estimators=50, criterion='gini', random_state=0)
-    metric = cross_val_score(RF,trainX_all,trainy_all, cv=10, scoring='f1',verbose = 1)
-    #[Parallel(n_jobs=1)]: Using backend SequentialBackend with 1 concurrent workers.
-    #[Parallel(n_jobs=1)]: Done  10 out of  10 | elapsed:  7.7min finished
-    metric.sort()
-    metric_all['RF50'] = metric[::-1]
+    #metric.sort()
+    metric_all['DecisionTree'] = metric[::-1]
 
     from sklearn.naive_bayes import GaussianNB
     gnb = GaussianNB()
     metric = cross_val_score(gnb, trainX_all,trainy_all, cv=10, scoring='f1',verbose = 1)
     #[Parallel(n_jobs=1)]: Using backend SequentialBackend with 1 concurrent workers.
     #[Parallel(n_jobs=1)]: Done  10 out of  10 | elapsed:   24.8s finished
-    metric.sort()
-    metric_all['gnb'] = metric[::-1]
+    #metric.sort()
+    metric_all['GaussianNB'] = metric[::-1]
 
     from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
     lda = LinearDiscriminantAnalysis(solver='lsqr', shrinkage=None, priors=None)
     metric = cross_val_score(lda, trainX_all,trainy_all, cv=10, scoring='f1',verbose = 1)
     #[Parallel(n_jobs=1)]: Using backend SequentialBackend with 1 concurrent workers.
     #[Parallel(n_jobs=1)]: Done  10 out of  10 | elapsed:   42.5s finished
-    metric.sort()
-    metric_all['lda'] = metric[::-1]
+    #metric.sort()
+    metric_all['LDA'] = metric[::-1]
 
     from sklearn.ensemble import GradientBoostingClassifier
-    gb = GradientBoostingClassifier(n_estimators=100, learning_rate=1.0,max_depth=1, random_state=0)
+    gb = GradientBoostingClassifier(n_estimators=500,validation_fraction = 0.2,tol = 0.01,learning_rate=0.1,\
+                                     min_samples_split=20,max_features='sqrt',subsample=0.8,random_state=10)
     metric = cross_val_score(lda, trainX_all,trainy_all, cv=10, scoring='f1',verbose = 1)
     #[Parallel(n_jobs=1)]: Using backend SequentialBackend with 1 concurrent workers.
     #[Parallel(n_jobs=1)]: Done  10 out of  10 | elapsed:   39.8s finished
-    metric.sort()
-    metric_all['gb'] = metric[::-1]
+    #metric.sort()
+    metric_all['GradientBoosting'] = metric[::-1]
+
+    from xgboost.sklearn import XGBClassifier
+    xg = XGBClassifier(learning_rate=0.1, n_estimators=1000, max_depth=6,eta = 0.1,\
+                             objective="binary:logistic", subsample=0.9, colsample_bytree=0.8)
+    metric = cross_val_score(xg,np.array(trainX_all),
+                             np.array(trainy_all), cv=10, scoring='f1',verbose = 1)
+    metric_all['xgboosting'] = metric[::-1]
     print(metric_all)
     metric_mean = metric_all.mean()
     print(metric_mean.sort_values(ascending=False))
